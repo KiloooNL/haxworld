26c9c6dda6f4c7433a1919321d4b47d0b9e458f7
KiloooNL <benweidenhofer@live.com>
2017-04-25 16:20:33 +0930

    Added r12

---
+++ windows/haxworld/bin/Debug/animation.d
@@ -0,0 +1,3 @@
+/home/ben/Projects/haxworld/haxworld/bin/Debug/animation.o: \
+ /home/ben/Projects/haxworld/haxworld/animation.cpp \
+ /home/ben/Projects/haxworld/haxworld/define.h
---
+++ windows/haxworld/bin/Debug/framerate_test.d
@@ -0,0 +1,3 @@
+/home/ben/Projects/haxworld/haxworld/bin/Debug/framerate_test.o: \
+ /home/ben/Projects/haxworld/haxworld/framerate_test.c \
+ /home/ben/Projects/haxworld/haxworld/define.h
